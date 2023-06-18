6d1f37bb9e96e9e04e5789cecc9e355071c27b7d
IrinaSerpokrylova <ira.serpokrylova@yandex.ru>
2023-06-18 23:52:09 +0300

    удален пустой файл

--- utils/validate.js
+++
@@ -1,9 +0,0 @@
-const checkLength = (errors) => {
-  const message = { message: '' };
-  errors.forEach((error) => {
-    if (error.properties.type === 'minlength') {
-      message.message.concat(``);
-    } else if (error.properties.type === 'maxlength') {
-    }
-  });
-};
