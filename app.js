ec5de75b28304a2a459b6756641fab6f838152c3
IrinaSerpokrylova <ira.serpokrylova@yandex.ru>
2023-06-18 18:22:57 +0300

    ПР 13 - первый коммит

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routesCard = require('./routes/cards');
+const routesUsers = require('./routes/users');
+
+const { PORT = 3000 } = process.env;
+
+mongoose
+  .connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+  })
+  .then(() => {
+    console.log('connected to db');
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '648dd61dc4c4ead7281f98fa', //  _id созданного пользователя
+  };
+
+  next();
+});
+
+app.use(routesUsers);
+app.use('/cards', routesCard);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ controllers/users.js
@@ -0,0 +1,91 @@
+const User = require('../models/user');
+
+const getUsers = (req, res, next) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Произошла ошибка' });
+      next();
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = req.params.userId;
+  return User.findById(id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(500).send({
+          message: 'Переданы некорректные данные при обновлении профиля',
+        });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(500).send({
+          message: 'Переданы некорректные данные при обновлении профиля',
+        });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUserProfile,
+  updateUserAvatar,
+};
