838673e341f0124bbc553edc8471c9ab5ae8c5a8
IrinaSerpokrylova <118003599+IrinaSerpokrylova@users.noreply.github.com>
2023-07-04 00:40:06 +0300

    Update statuses.js

--- utils/statuses.js
+++ utils/statuses.js
@@ -2,10 +2,12 @@ const badRequestError = 400;
 const notFoundError = 404;
 const internalServerError = 500;
 const statusOK = 200;
+const created = 201;
 
 module.exports = {
   badRequestError,
   notFoundError,
   internalServerError,
   statusOK,
+  created,
 };
